af87b529f00fe014561a38af94cc4c9235780e9f
Vsim <73603125+Vsimpro@users.noreply.github.com>
2022-01-19 09:18:38 +0200

    fixed server status bug

--- src/output.py
+++ src/output.py
@@ -13,21 +13,22 @@ def create_json(line,path):
         # Server Status
     if 'For help, type "help"' in line:
         server_status["Status"] = "ONLINE"
-        
+
     if 'INFO]: Stopping' in line:
         server_status["Status"] = "OFFLINE"
-        
+
         # Player Joined
     if "the game" in line:
         player = line.split(" ")[3]
         action = line.split(" ")[4]
 
         if action == "joined":
-            online_status.append(player)
+            if not player in online_status:
+                online_status.append(player)
 
         if action == "left":
-            online_status.remove(player)
+            if player in online_status:
+                online_status.remove(player)
 
-    
     with open(f"{path}server_status.json","w+") as file:
         json.dump(server_status, file)
